915514603e23e363f87a8f2e920b9240e2a82c0c
Victimnn <126915227+victimnn@users.noreply.github.com>
2024-10-27 19:25:05 -0300

    Update index.py

--- index.py
+++ index.py
@@ -3,7 +3,9 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.prompts import PromptTemplate
 import os
 
-os.environ['GOOGLE_API_KEY'] = 'GOOGLE_API_KEY'
+GOOGLE_API_KEY = st.secrets["general"]["GOOGLE_API_KEY"]
+
+os.environ['GOOGLE_API_KEY'] = GOOGLE_API_KEY
 
 llm = ChatGoogleGenerativeAI(model='gemini-1.5-pro', temperature=0)
 
