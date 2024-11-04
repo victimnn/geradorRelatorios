4536e5bbe1966617ab28496351bcd6a97e672654
Victimnn <126915227+victimnn@users.noreply.github.com>
2024-11-04 07:43:15 -0300

    Update index.py

--- index.py
+++ index.py
@@ -56,7 +56,7 @@ analises = [
     "Análise de Posição de Mercado"
 ]
 
-st.title('📝 Gerador de Relatórios Financeiros')
+st.title('📝Gerador de Relatórios Financeiros')
 
 st.sidebar.header('Configurações do Relatório')
 empresa = st.sidebar.selectbox('Selecione a empresa:', empresas)
